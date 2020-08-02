fa70dc5d4527e346f61a6dcb6c40abe318f3c811
Paul Bittner <paul.bittner@uni-ulm.de>
2020-08-02 21:33:59 +0200

    indentation

--- SharpCategory/Challenge7/Reader.cs
+++ SharpCategory/Challenge7/Reader.cs
@@ -5,7 +5,7 @@
         public static Function<Function<R, A>, Function<R, B>> fmap<A, B>(Function<A, B> f) 
             => reader => Challenge1.Functions.Composition(f, reader);
 
-        public static Function<R, B> fmap_curried<A, B>(Function<R, A> reader, Function<A, B> f)
-         => Challenge1.Functions.Composition(f, reader);
+        public static Function<R, B> fmap_curried<A, B>(Function<R, A> reader, Function<A, B> f) 
+            => Challenge1.Functions.Composition(f, reader);
     }
 }
